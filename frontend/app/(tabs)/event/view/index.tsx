573e8eb9136ed84954efdbfe25e09520138d7430
Nagalin <augustnagalin@gmail.com>
2025-01-24 01:19:34 +0700

    feat: add error snackbar for event data fetching
    - add snackbars to display error messages when event data fetching fails

--- frontend/app/(tabs)/event/view/index.tsx
+++ frontend/app/(tabs)/event/view/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Background from '@/components/ui/background'
 import EventCard from '@/components/event/view/event-card'
 import CreateEventModal from '@/components/event/create/create-event-modal'
-
+import { Snackbar } from 'react-native-paper';
 type Event = {
   id: string
   name: string
@@ -11,6 +11,9 @@ type Event = {
 }
 
 const Index = () => {
+  const [visible, setVisible] = React.useState(true);
+
+  const onDismissSnackBar = () => setVisible(false);
   const mockedData: Event[] = [
     { id: '1', name: 'Event 1', date: new Date('2064-01-10') },
     { id: '2', name: 'Event 2', date: new Date('2066-01-12') },
@@ -37,6 +40,19 @@ const Index = () => {
 
       </ScrollView>
       <CreateEventModal />
+      <Snackbar
+        style={{ backgroundColor: 'red' }}
+        duration={9999}
+        visible={visible}
+        onDismiss={onDismissSnackBar}
+        action={{
+          label: 'Try again',
+          onPress: () => {
+            console.log('clicked')
+          },
+        }}>
+        Something went wrong
+      </Snackbar>
     </Background>
   )
 }
